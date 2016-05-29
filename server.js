0536cfed5472aefd87bd2cd6cb57a7ed7bfc5d3c
Rajat Mhetre <mhetrerajat@yahoo.in>
2016-05-29 21:28:00 +0530

    update public path

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ module.exports = {
   app: function () {
     const app = express()
     const indexPath = path.join(__dirname, './index.html')
-    //const publicPath = express.static(path.join(__dirname, '../public'))
+    const publicPath = express.static(path.join(__dirname, './public'))
 
-    //app.use('/public', publicPath)
+    app.use('/', publicPath)
     app.get('/', function (_, res) { res.sendFile(indexPath) })
 
     return app
